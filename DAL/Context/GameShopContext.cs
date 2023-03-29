8733879333e8f84858fe12906120627141d851c7
Maksym Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-03-29 23:19:21 +0100

    added dbcontext and started adding fluent configs

---
+++ DAL/Context/GameShopContext.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Context
+{
+    public class GameShopContext : DbContext
+    {
+        DbSet<Coment> Coments { get; set; }
+        DbSet<Game> Games { get; set; }
+        DbSet<GameGenre> GameGenres { get; set; }
+        DbSet<GamePlatformType> GamePlatformTypes { get; set; }
+        DbSet<Genre> Genres { get; set; }
+        DbSet<PlatformType> PlatformTypes { get; set; }
+
+        public GameShopContext() : base("name=DefaultConnectingString")
+        {
+            Database.SetInitializer<GameShopContext>(new DataSeeder());
+        }
+    }
+}
---
+++ DAL/DataSeeder.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataSeeder : DropCreateDatabaseIfModelChanges<GameShopContext>
+    {
+        protected override void Seed(GameShopContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
