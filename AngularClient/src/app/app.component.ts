29fa12451d37fc9852df8e675226f41f22686174
Maksym Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-04-19 18:45:57 +0200

    GMSTR-3: Added Angular client project to root directory.

---
+++ AngularClient/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Game shop';
+  games: any;
+
+  constructor(private http: HttpClient) {}
+  addGame(): void {
+    this.games = this.http
+      .get('/api/games/getAll')
+      .subscribe((data) => {
+        this.games = data;
+      });
+  }
+}
